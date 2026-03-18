c8c2e2870084838c4fb6a41376b9cffdccc54d1a
al3ab1020 <al3ab1020@gmail.com>
2026-03-19 01:31:42 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     console.log("🔗 LINK:", link);
 
     if (!link) {
-      console.log("❌ ما حصلنا الرابط");
+      console.log("رابط الحساب");
       return res.status(200).json({ message: "no link" });
     }
 
