27eae1e2305db4890043eed22d1cff704c2a9286
al3ab1020 <al3ab1020@gmail.com>
2026-03-19 00:32:28 +0300

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,37 @@
+export default async function handler(req, res) {
+  try {
+
+    const data = req.body;
+
+    // تأكد أن الطلب مدفوع
+    if (data.status === "paid") {
+
+      // بيانات العميل من رمز
+      const link = data.meta?.link;
+      const quantity = data.meta?.quantity;
+
+      // إرسال الطلب إلى drd3m
+      await fetch("https://drd3m.me/api/v2", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          key: "71b3dc3bb5c30df01628722729f22a0d", // 
+          action: "add",
+          service: 9489, // 
+          link: link,
+          quantity: quantity
+        })
+      });
+
+      console.log("تم إرسال الطلب ✅");
+    }
+
+    res.status(200).json({ success: true });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "error" });
+  }
+}
